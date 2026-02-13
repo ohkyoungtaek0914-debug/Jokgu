34b2d41a40748dcadc477449333e598f205f73bd
Kyoungtaek Oh <ohkyoungtaek0914@gmail.com>
2026-02-13 11:53:34 +0900

    update: 변경 내용 요약

--- MainWindow.h
+++ MainWindow.h
@@ -1,6 +1,8 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "TeamMaker.h"
+
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -8,6 +10,8 @@ namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
+
+class QListWidgetItem;
 
 class MainWindow : public QMainWindow
 {
@@ -17,7 +21,18 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void onFilterChanged(const QString& text);
+    void onSelectAllVisible();
+    void onUnselectAllVisible();
+    void onMakeTeams();
+
 private:
+    void loadPlayers();
+    void applyFilter(const QString& text);
+
     Ui::MainWindow *ui;
+    TeamMaker m_teamMaker;
 };
+
 #endif // MAINWINDOW_H
